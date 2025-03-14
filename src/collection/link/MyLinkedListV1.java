95bcedd2a1d24b5b0f74d5943a3051d898c4bbbd
Teo_You <teoyou881@gmail.com>
2025-03-14 17:38:15 -0400

    Collection - LinkedList

---
+++ src/collection/link/MyLinkedListV1.java
@@ -0,0 +1,101 @@
+package collection.link;
+
+public class MyLinkedListV1 {
+  private Node first; // Head node of the linked list
+  private int size = 0; // Number of elements in the list
+
+  // Adds an element to the end of the list
+  public void add(Object e) {
+    Node newNode = new Node(e);
+    if (first == null) {
+      first = newNode;
+    } else {
+      Node lastNode = getLastNode();
+      lastNode.next = newNode;
+    }
+    size++;
+  }
+
+  // Retrieves the last node in the linked list
+  private Node getLastNode() {
+    Node x = first;
+    while (x.next != null) {
+      x = x.next;
+    }
+    return x;
+  }
+
+  // Replaces an element at a given index and returns the old value
+  public Object set(int index, Object element) {
+    Node x = getNode(index);
+    Object oldValue = x.item;
+    x.item = element;
+    return oldValue;
+  }
+
+  // Retrieves an element at a given index
+  public Object get(int index) {
+    Node node = getNode(index);
+    return node.item;
+  }
+
+  // Retrieves the node at a specific index
+  private Node getNode(int index) {
+    Node x = first;
+    for (int i = 0; i < index; i++) {
+      x = x.next;
+    }
+    return x;
+  }
+
+  // Returns the index of the first occurrence of the given element, or -1 if not found
+  public int indexOf(Object o) {
+    int index = 0;
+    for (Node x = first; x != null; x = x.next) {
+      if (o.equals(x.item))
+        return index;
+      index++;
+    }
+    return -1;
+  }
+
+  // Returns the size of the list
+  public int size() {
+    return size;
+  }
+
+  // Returns a string representation of the linked list
+  @Override
+  public String toString() {
+    return "MyLinkedListV1{" +
+        "first=" + first +
+        ", size=" + size +
+        '}';
+  }
+
+  // Node class representing each element in the linked list
+  private static class Node {
+    Object item;
+    Node next;
+
+    public Node(Object item) {
+      this.item = item;
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+      Node temp = this;
+      sb.append("[");
+      while (temp != null) {
+        sb.append(temp.item);
+        if (temp.next != null) {
+          sb.append("->");
+        }
+        temp = temp.next;
+      }
+      sb.append("]");
+      return sb.toString();
+    }
+  }
+}
