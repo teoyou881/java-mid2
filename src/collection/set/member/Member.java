8694b58efde2d0fe1061c885603c676915574dde
Teo_You <teoyou881@gmail.com>
2025-03-16 00:05:49 -0400

    Collection - hash algorithm When we use hash stuff, we should override both hashcode and equals methods

---
+++ src/collection/set/member/Member.java
@@ -0,0 +1,43 @@
+package collection.set.member;
+
+import java.util.Objects;
+
+public class Member {
+
+  private String id;
+
+  public Member(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    Member member = (Member) obj;
+    return Objects.equals(id, member.id);
+  }
+
+
+  @Override
+  public String toString() {
+    return "Member{" +
+        "id='" + id + '\'' +
+        '}';
+  }
+}
+
+
